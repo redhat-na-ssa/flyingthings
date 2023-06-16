fb4fd6446507e87b959d9ab937ec280cd1b7816b
David White <davwhite@davwhite.com>
2023-06-16 09:36:48 -0400

    model deployment

--- deployment/01-build-model.sh
+++ deployment/01-build-model.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-oc new-build --name flyingthings-model --strategy docker --binary --context-dir .
+oc new-build --name yolo --strategy docker --binary --context-dir .
+oc start-build yolo --from-dir yolo --follow
+
+oc new-build --name flyingthings-model --strategy docker --binary --image-stream yolo:latest --context-dir .
 oc start-build flyingthings-model --from-dir model --follow
