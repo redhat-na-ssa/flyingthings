1bbcb4ad2467f12edaf56bf54781210020232afa
David White <davwhite@davwhite.com>
2023-06-18 17:25:37 -0400

    build and deployment of custom and pretrained models

--- deployment/01-build-model.sh
+++ deployment/01-build-model.sh
@@ -2,5 +2,8 @@
 oc new-build --name yolo --strategy docker --binary --context-dir .
 oc start-build yolo --from-dir yolo --follow
 
-oc new-build --name flyingthings-model --strategy docker --binary --image-stream yolo:latest --context-dir .
-oc start-build flyingthings-model --from-dir model --follow
+oc new-build --name custom-model --strategy docker --binary --image-stream yolo:latest --env="WEIGHTS=model_custom.pt" --context-dir .
+oc start-build custom-model --from-dir model --follow
+
+oc new-build --name coco-model --strategy docker --binary --image-stream yolo:latest --env="WEIGHTS=model_pretrained.pt" --context-dir .
+oc start-build coco-model --from-dir model --follow
