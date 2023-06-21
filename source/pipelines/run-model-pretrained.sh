f441ea6c2766f4df586e6080a767b1a9dfc51cb8
Super Dave <davwhite@davwhite.com>
2023-06-21 19:18:25 -0400

    pipeline runs

--- source/pipelines/run-model-pretrained.sh
+++ source/pipelines/run-model-pretrained.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
+
+# Check the number of arguments
+if [ "$#" -ne 3 ]; then
+  echo "Error: Expected 3 arguments, but received $# arguments."
+  echo "Usage: $0 NAMESPACE MINIO_ENDPOINT MINIO_BUCKET"
+  exit 1
+fi
+
+NAMESPACE=$1
+MINIO_ENDPOINT=$2
+MINIO_BUCKET=$3
+
 tkn pipeline start app-pipeline \
 -w name=shared-workspace,\
 volumeClaimTemplateFile=https://raw.githubusercontent.com/openshift/pipelines-tutorial/pipelines-1.8/01_pipeline/03_persistent_volume_claim.yaml \
 -p deployment-name=model-server-pretrained \
 -p git-url=https://github.com/davwhite/flyingthings.git \
 -p git-revision=develop \
--p BUILD_EXTRA_ARGS='--build-arg WEIGHTS=model_pretrained.pt --build-arg MODEL_CLASSES=flyingthings.yaml --build-arg BUILD_VER=0.0 --build-arg MINIO_ENDPOINT=https://minio-flyingthings-standalone.apps.ocp4.davenet.local/flyingthings --build-arg SIMPLEVIS_DATA=/opt/app-root/src/simplevis-data' \
--p IMAGE=image-registry.openshift-image-registry.svc:5000/flyingthings-standalone/pretrained-model \
+-p BUILD_EXTRA_ARGS='--build-arg WEIGHTS=model_pretrained.pt --build-arg MODEL_CLASSES=flyingthings.yaml --build-arg BUILD_VER=0.0 --build-arg MINIO_ENDPOINT=$MINIO_ENDPOINT/$MINIO_BUCKET --build-arg SIMPLEVIS_DATA=/opt/app-root/src/simplevis-data' \
+-p IMAGE=image-registry.openshift-image-registry.svc:5000/$NAMESPACE/pretrained-model \
 --use-param-defaults
