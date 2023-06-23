0515dd4ff0f984905d32cb54ec09cf752d7a02f3
Super Dave <davwhite@davwhite.com>
2023-06-22 22:04:46 -0400

    Added results output to training pipeline

--- source/training/push-results.sh
+++ source/training/push-results.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 cd /opt/app-root/src/simplevis-data/
+echo "*************** Training Run Results*************************"
+cat /opt/app-root/src/simplevis-data/runs/detect/train/results.csv
+echo "************************************************************"
 tar czf training-results.tgz /opt/app-root/src/simplevis-data/runs/detect/train/
 ./mc --config-dir miniocfg cp training-results.tgz myminio/flyingthings/training-results.tgz --insecure
 ./mc --config-dir miniocfg cp /opt/app-root/src/simplevis-data/runs/detect/train/weights/best.pt myminio/flyingthings/model_custom.pt --insecure
