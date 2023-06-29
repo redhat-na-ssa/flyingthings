41850372ab49f4c9b839ba67135df21b96c06f5d
David White <davwhite@davwhite.com>
2023-06-29 06:21:42 -0400

    Replaced scripts with pipelines

--- standalone/02-deploy-artifacts.sh
+++ standalone/02-deploy-artifacts.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-MINIO_ROUTE=`oc get route|grep -v console|grep -v NAME|awk '{print $2 }'`
-python ../scripts/01-create-bucket.py $MINIO_ROUTE minioadmin minioadmin
-python ../scripts/02-enable-versioning.py $MINIO_ROUTE minioadmin minioadmin
-python ../scripts/03-upload-artifacts.py $MINIO_ROUTE minioadmin minioadmin
-python ../scripts/04-tag-objects.py $MINIO_ROUTE minioadmin minioadmin
+
+tkn pipeline start minio-server-setup \
+-w name=shared-workspace,\
+volumeClaimTemplateFile=../source/pipelines/custom-pvc.yaml \
+-p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
+-p git-revision=main \
+--use-param-defaults
