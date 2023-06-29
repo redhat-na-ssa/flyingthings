b477d9f282c0986e8e159dd133bd7a18ed855db7
David White <davwhite@davwhite.com>
2023-06-29 18:15:09 -0400

    minio client run

--- standalone/02-deploy-artifacts.sh
+++ standalone/02-deploy-artifacts.sh
@@ -2,7 +2,8 @@
 
 tkn pipeline start minio-server-setup \
 -w name=shared-workspace,\
-volumeClaimTemplateFile=../source/pipelines/custom-pvc.yaml \
+volumeClaimTemplateFile=custom-pvc.yaml \
 -p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
 -p git-revision=main \
---use-param-defaults
+--use-param-defaults \
+--use-param-defaults --showlog
