552848d9058bea3112e6ef64c7c9eaa936631632
David White <davwhite@davwhite.com>
2023-06-29 18:13:59 -0400

    minio client run

--- source/pipelines/02-run-minio-client-pipeline.sh
+++ source/pipelines/02-run-minio-client-pipeline.sh
@@ -9,4 +9,5 @@ tkn pipeline start minio-server-setup \
 volumeClaimTemplateFile=custom-pvc.yaml \
 -p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
 -p git-revision=main \
---use-param-defaults
+--use-param-defaults \
+--use-param-defaults --showlog
