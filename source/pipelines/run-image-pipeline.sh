19794cc9d9a32a2dff9a602cef845fcc919546ea
David White <davwhite@davwhite.com>
2023-06-29 12:38:39 -0400

    image pipeline

--- source/pipelines/run-image-pipeline.sh
+++ source/pipelines/run-image-pipeline.sh
@@ -8,7 +8,7 @@ tkn pipeline start flyingthings-image-pipeline \
 -w name=shared-workspace,\
 volumeClaimTemplateFile=custom-pvc.yaml \
 -p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
--p git-revision=bootpipeline \
--p IMAGE=image-registry.openshift-image-registry.svc:5000/flyingthings-standalone/ubi9 \
--p DOCKERFILE_PATH=source/ubi9 \
+-p git-revision=develop \
+-p IMAGE=yolo \
+-p DOCKERFILE_PATH=source/yolo \
 --use-param-defaults
