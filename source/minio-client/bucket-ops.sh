f2c8da00689050afc5e5ca8a902e11df598b3a23
David White <davwhite@davwhite.com>
2023-06-29 05:13:07 -0400

    updated minio batch file

--- source/minio-client/bucket-ops.sh
+++ source/minio-client/bucket-ops.sh
@@ -6,6 +6,7 @@ MINIO_SECRETKEY=minioadmin
 MINIO_CLIENT_URL=https://dl.min.io/client/mc/release/linux-amd64
 WORKSPACE_DIR=/opt/app-root/src/workspace
 MINCFG=$WORKSPACE_DIR/miniocfg
+SOURCE_DIR=/workspace/output
 
 RUN mkdir -p ${WORKSPACE_DIR}
 cd $SIMPLEVIS_DATA
@@ -13,5 +14,14 @@ ls -l $SIMPLEVIS_DATA
 curl $MINIO_CLIENT_URL/mc -o mc
 chmod +x mc
 ./mc --config-dir ${MINCFG} config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
-./mc --config-dir ${MINCFG} cp $SIMPLEVIS_DATA/runs/training-results.tgz myminio/$MINIO_BUCKET/training-results.tgz --insecure
-./mc --config-dir ${MINCFG} cp $SIMPLEVIS_DATA/runs/train/weights/best.pt myminio/$MINIO_BUCKET/model_candidate.pt --insecure
+./mc --config-dir ${MINCFG} mb myminio/$MINIO_BUCKET --insecure
+./mc --config-dir ${MINCFG} version enable myminio/$MINIO_BUCKET --insecure
+./mc --config-dir ${MINCFG} cp --recursive $SOURCE_DIR/artifacts myminio/$MINIO_BUCKET --insecure
+
+# List all objects in the bucket
+objects=$(mc ls --recursive $MINIO_BUCKET | awk '{ print $5 }')
+
+# Loop through each object and apply tags
+for object in $objects; do
+  mc cp --attr "build=0.0" $MINIO_BUCKET/$object $MINIO_BUCKET/$object
+done
