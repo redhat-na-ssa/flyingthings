3ce9def965405887c7e91ee6cd1223a3c844d8b7
David White <davwhite@davwhite.com>
2023-06-29 04:54:28 -0400

    updated minio batch file

--- source/minio-client/bucket-ops.sh
+++ source/minio-client/bucket-ops.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
+MINIO_BUCKET=flyingthings
+MINIO_ACCESSKEY=minioadmin
+MINIO_SECRETKEY=minioadmin
+MINIO_CLIENT_URL=https://dl.min.io/client/mc/release/linux-amd64
+WORKSPACE_DIR=/opt/app-root/src/workspace
+MINCFG=$WORKSPACE_DIR/miniocfg
+
+RUN mkdir -p ${WORKSPACE_DIR}
 cd $SIMPLEVIS_DATA
 ls -l $SIMPLEVIS_DATA
 curl $MINIO_CLIENT_URL/mc -o mc
 chmod +x mc
 ./mc --config-dir ${MINCFG} config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
-./mc --config-dir ${MINCFG} cp $SIMPLEVIS_DATA/runs/training-results.tgz myminio/flyingthings/training-results.tgz --insecure
-./mc --config-dir ${MINCFG} cp $SIMPLEVIS_DATA/runs/train/weights/best.pt myminio/flyingthings/model_candidate.pt --insecure
+./mc --config-dir ${MINCFG} cp $SIMPLEVIS_DATA/runs/training-results.tgz myminio/$MINIO_BUCKET/training-results.tgz --insecure
+./mc --config-dir ${MINCFG} cp $SIMPLEVIS_DATA/runs/train/weights/best.pt myminio/$MINIO_BUCKET/model_candidate.pt --insecure
