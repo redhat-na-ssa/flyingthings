565ff563c18270452ee0a4c7d2ad77b2116938a0
David White <davwhite@davwhite.com>
2023-06-29 22:18:28 -0400

    model srv

--- source/pipelines/run-model-custom.sh
+++ source/pipelines/run-model-custom.sh
@@ -4,12 +4,13 @@ MINIO_ENDPOINT=`oc get route|grep -v console|grep -v NAME|awk '{print $2 }'`
 NAMESPACE='flyingthings-standalone'
 MINIO_BUCKET='flyingthings'
 
-tkn pipeline start app-pipeline \
+tkn pipeline start model-serv-pipeline \
 -w name=shared-workspace,\
 volumeClaimTemplateFile=custom-pvc.yaml \
 -p deployment-name=model-server-custom \
--p git-url=https://github.com/davwhite/flyingthings.git \
--p git-revision=main \
--p BUILD_EXTRA_ARGS='--build-arg WEIGHTS=model_custom.pt --build-arg MODEL_CLASSES=flyingthings.yaml --build-arg BUILD_VER=0.0 --build-arg MINIO_ENDPOINT=$MINIO_ENDPOINT/$MINIO_BUCKET --build-arg SIMPLEVIS_DATA=/opt/app-root/src/simplevis-data' \
--p IMAGE=image-registry.openshift-image-registry.svc:5000/$NAMESPACE/custom-model \
---use-param-defaults
+-p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
+-p git-revision=develop \
+-p BUILD_EXTRA_ARGS='--build-arg WEIGHTS=model_custom.pt --build-arg MINIO_ENDPOINT=http://minio:9000 --build-arg MINIO_BUCKET=flyingthings --build-arg SIMPLEVIS_DATA=/opt/app-root/src/simplevis-data' \
+-p IMAGE=image-registry.openshift-image-registry.svc:5000/flyingthings-standalone/custom-model \
+--use-param-defaults \
+--use-param-defaults --showlog
