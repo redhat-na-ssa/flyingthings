03b8700ff5ec8a788ab81ef8c0ec6625e620b47c
David White <davwhite@davwhite.com>
2023-06-30 23:50:29 -0400

    pipeline runs

--- bootstrap/01-create-pipelines.sh
+++ bootstrap/01-create-pipelines.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 oc new-project flyingthings-standalone
+
 cd ../pipelines/manifests
+oc apply -f .
+
+cd ../pipelines/runs
 oc apply -f .
--- bootstrap/run-model-custom.sh
+++ bootstrap/run-model-custom.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
 
-MINIO_ENDPOINT=`oc get route|grep -v console|grep -v NAME|awk '{print $2 }'`
-NAMESPACE='flyingthings-standalone'
-MINIO_BUCKET='flyingthings'
+cd ../pipelines/runs
 
-tkn pipeline start model-serv-pipeline \
--w name=shared-workspace,\
+tkn pipeline start deploy-model-server \
+-w name=workspace,\
 volumeClaimTemplateFile=custom-pvc.yaml \
--p deployment-name=model-server-custom \
--p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
--p git-revision=develop \
--p BUILD_EXTRA_ARGS='--build-arg WEIGHTS=model_custom.pt --build-arg MINIO_ENDPOINT=http://minio:9000 --build-arg MINIO_BUCKET=flyingthings --build-arg SIMPLEVIS_DATA=/opt/app-root/src/simplevis-data' \
--p IMAGE=image-registry.openshift-image-registry.svc:5000/flyingthings-standalone/custom-model \
---use-param-defaults \
+-p GIT_REPO=https://github.com/redhat-na-ssa/flyingthings.git \
+-p GIT_REVISION=develop \
+-p IMAGE=image-registry.openshift-image-registry.svc:5000/flyingthings-standalone/custom-model:latest \
+-p DOCKERFILE_PATH=source \
+-p BUILD_EXTRA_ARGS='--build-arg WEIGHTS=flyingthings.pt \
+--build-arg BUILD_VER=0.0 \
+--build-arg MODEL_CLASSES=flyingthings.yaml \
+--build-arg SIMPLEVIS_DATA=/opt/app-root/src/simplevis-data \
+--build-arg BASEDIR=/opt/app-root/src \
+--build-arg MINIO_ENDPOINT=http://minio:9000 \
+--build-arg MINIO_BUCKET=flyingthings \
+--build-arg MINIO_ACCESSKEY=minioadmin \
+--build-arg MINIO_SECRETKEY=minioadmin' \
 --use-param-defaults --showlog
