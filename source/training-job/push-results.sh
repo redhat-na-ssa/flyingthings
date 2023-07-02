1a3e227bea3790309843d6ddf9f691c34fbd8883
David White <davwhite@davwhite.com>
2023-07-02 11:33:16 -0400

    corrected issue with training job push

--- source/training-job/push-results.sh
+++ source/training-job/push-results.sh
@@ -9,4 +9,4 @@ ls -l $SIMPLEVIS_DATA
 ./mc --config-dir miniocfg config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
 ./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/training-results.tgz myminio/flyingthings/training-results.tgz --insecure
 ./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/flyingthings/model_candidate.pt --insecure
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/flyingthings/model_custon.pt --insecure
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/flyingthings/model_custom.pt --insecure
