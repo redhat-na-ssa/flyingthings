b0e0a2abc16f044400a87f6c19c6d8b787932bc8
David White <davwhite@davwhite.com>
2023-07-04 23:01:56 -0400

    single pipeline

--- source/minio-client/bucket-ops.sh
+++ source/minio-client/bucket-ops.sh
@@ -18,3 +18,6 @@ chmod +x mc
 ./mc --config-dir ${MINCFG} version enable myminio/$MINIO_BUCKET --insecure
 ./mc --config-dir ${MINCFG} cp $SOURCE_DIR/artifacts/flyingthings.yaml myminio/$MINIO_BUCKET --insecure
 ./mc --config-dir ${MINCFG} cp $SOURCE_DIR/artifacts/flyingthings-yolo.zip myminio/$MINIO_BUCKET --insecure
+./mc --config-dir ${MINCFG} cp $SOURCE_DIR/notebooks/01-training-prep.ipynb myminio/$MINIO_BUCKET --insecure
+./mc --config-dir ${MINCFG} cp $SOURCE_DIR/notebooks/02-object-detect-train.ipynb myminio/$MINIO_BUCKET --insecure
+./mc --config-dir ${MINCFG} cp $SOURCE_DIR/notebooks/99-utils.ipynb myminio/$MINIO_BUCKET --insecure
