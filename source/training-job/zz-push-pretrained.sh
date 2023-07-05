e7ca49b9129ef9f28c7fcea23fd73680371cff65
David White <davwhite@davwhite.com>
2023-07-05 15:07:06 -0400

    pretrained model

--- source/training-job/zz-push-pretrained.sh
+++ source/training-job/zz-push-pretrained.sh
@@ -4,5 +4,5 @@ yolo predict model=yolov8n.pt
 curl $MINIO_CLIENT_URL/mc -o mc
 chmod +x mc
 ./mc --config-dir miniocfg config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
-./mc --config-dir miniocfg cp /workspace/yolov8n.py myminio/flyingthings/model_pretrained.pt --insecure
-./mc --config-dir miniocfg cp /workspace/yolov8n.py myminio/flyingthings/model_custom.pt --insecure
+./mc --config-dir miniocfg cp /workspace/yolov8n.pt myminio/flyingthings/model_pretrained.pt --insecure
+./mc --config-dir miniocfg cp /workspace/yolov8n.pt myminio/flyingthings/model_custom.pt --insecure
