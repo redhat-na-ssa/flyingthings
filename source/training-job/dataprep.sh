0edd61cee4ea6d134937f636c3c22ef767d57db0
David White <davwhite@davwhite.com>
2023-07-07 18:26:45 -0400

    updated training jog to name from params

--- source/training-job/dataprep.sh
+++ source/training-job/dataprep.sh
@@ -5,8 +5,15 @@ chmod +x mc
 ./mc --config-dir miniocfg config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
 ./mc --config-dir miniocfg cp myminio/$MINIO_BUCKET/$MODEL_CLASSES $MODEL_CLASSES --insecure
 ./mc --config-dir miniocfg cp myminio/$MINIO_BUCKET/$WEIGHTS $WEIGHTS --insecure
-./mc --config-dir miniocfg cp myminio/$MINIO_BUCKET/$BASE_MODEL $BASE_MODEL --insecure
 ./mc --config-dir miniocfg cp myminio/$MINIO_BUCKET/$DATASET_ZIP $DATASET_ZIP --insecure
+
+# Use the default yolo if no custom base model is specified "yolov8n.pt"
+if [ "$BASE_MODEL" == "yolov8n.pt" ]; then
+    echo "Using pretrained model 'yolov8n.pt'"
+else
+    echo "Using supplied custom model $BASE_MODEL"
+    ./mc --config-dir miniocfg cp myminio/$MINIO_BUCKET/$BASE_MODEL $BASE_MODEL --insecure
+fi
 
 mkdir -p $SIMPLEVIS_DATA/workspace/datasets
 cd $SIMPLEVIS_DATA/workspace/datasets
