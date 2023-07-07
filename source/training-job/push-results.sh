334dcb8d82559b2e29319ac43c09b8366f7c643a
David White <davwhite@davwhite.com>
2023-07-07 18:15:38 -0400

    updated training jog to name from params

--- source/training-job/push-results.sh
+++ source/training-job/push-results.sh
@@ -7,6 +7,5 @@ tar czf $SIMPLEVIS_DATA/workspace/runs/training-results.tgz $SIMPLEVIS_DATA/work
 ls -l $SIMPLEVIS_DATA
 
 ./mc --config-dir miniocfg config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/training-results.tgz myminio/flyingthings/training-results.tgz --insecure
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/flyingthings/model_candidate.pt --insecure
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/flyingthings/model_custom.pt --insecure
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/training-results.tgz myminio/$MINIO_BUCKET/training-run/training-results.tgz --insecure
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/$MINIO_BUCKET/training-run/$WEIGHTS --insecure
