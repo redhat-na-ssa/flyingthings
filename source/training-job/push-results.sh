d68c1b4ce08a500f38c86faec1184885e86bf500
David White <davwhite@davwhite.com>
2023-07-07 21:36:01 -0400

    push final model to model_custom.pt

--- source/training-job/push-results.sh
+++ source/training-job/push-results.sh
@@ -9,3 +9,6 @@ ls -l $SIMPLEVIS_DATA
 ./mc --config-dir miniocfg config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
 ./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/training-results.tgz myminio/$MINIO_BUCKET/training-run/training-results.tgz --insecure
 ./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/$MINIO_BUCKET/training-run/$WEIGHTS --insecure
+# TODO: Add a check to see if the training run was successful
+# TODO: modify model deployment to use name of new model
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/$MINIO_BUCKET/model_custom.pt --insecure
