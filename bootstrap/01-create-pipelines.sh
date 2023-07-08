ac7485e5b962c5112b81a0abd115d74a67723f4d
David White <davwhite@davwhite.com>
2023-07-08 05:20:39 -0400

    restored images build

--- bootstrap/01-create-pipelines.sh
+++ bootstrap/01-create-pipelines.sh
@@ -4,4 +4,4 @@ oc new-project flyingthings-standalone
 
 cd ../pipelines/manifests
 oc apply -f 00-flyingthings-images-pipeline.yaml
-oc apply -f 10-training-run.yaml
+oc apply -f 10-training-pipeline.yaml
