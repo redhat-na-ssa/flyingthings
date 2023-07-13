778f34c0c71b61d6a707d3eb2c679e60d9a55ec0
David White <davwhite@davwhite.com>
2023-07-13 00:24:52 -0400

    template for classfile

--- source/training/03-run-training.sh
+++ source/training/03-run-training.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd $SIMPLEVIS_DATA/workspace
-yolo train model=${SIMPLEVIS_DATA}/workspace/$BASE_MODEL batch=$BATCH_SIZE epochs=$NUM_EPOCHS data=$MODEL_CLASSES project=${SIMPLEVIS_DATA}/workspace/runs exist_ok=True
+yolo train model=${SIMPLEVIS_DATA}/workspace/$BASE_MODEL batch=$BATCH_SIZE epochs=$NUM_EPOCHS data=classes.yaml project=${SIMPLEVIS_DATA}/workspace/runs exist_ok=True
