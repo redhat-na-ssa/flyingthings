69c7f76d599a371b8d059b3a0692011c7816d750
David White <davwhite@davwhite.com>
2023-07-13 09:55:33 -0400

    minio buckets

--- source/minio-client/01-setup-client.sh
+++ source/minio-client/01-setup-client.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
-WORKSPACE_DIR=/opt/app-root/src/workspace
-MINCFG=$WORKSPACE_DIR/miniocfg
-# SOURCE_DIR=/workspace/workspace
 
-mkdir -p ${WORKSPACE_DIR}
-cd $SIMPLEVIS_DATA
-ls -l $SIMPLEVIS_DATA
+MINCFG=miniocfg
+
 curl $MINIO_CLIENT_URL/mc -o mc
 chmod +x mc
 ./mc --config-dir ${MINCFG} config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
