106b0be1c8798b3fb239090f8e900d6a9bd32271
Super Dave <davwhite@davwhite.com>
2023-07-14 10:49:28 -0400

    Image resizing

--- source/training/100-image-resize.py
+++ source/training/100-image-resize.py
@@ -3,38 +3,25 @@ import os
 import sys
 import cv2
 
-def resize_images(directory, new_size):
-    # Create a new folder called "originals"
-    originals_folder = os.path.join(directory, 'originals')
-    os.makedirs(originals_folder, exist_ok=True)
 
-    # Move original images to "originals" folder
+def resize_images(directory, new_width, new_height):
+    # Resize images in the specified directory
     for filename in os.listdir(directory):
         if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            original_path = os.path.join(directory, filename)
-            new_path = os.path.join(originals_folder, filename)
-            os.rename(original_path, new_path)
-
-    # Resize images in the current directory
-    for filename in os.listdir(directory):
-        if filename.lower().endswith(('.png', '.jpg', '.jpeg')):
-            image_path = os.path.join(directory, filename)
-            image = cv2.imread(image_path)
-            resized_image = cv2.resize(image, new_size, interpolation=cv2.INTER_AREA)
-            cv2.imwrite(image_path, resized_image)
+            # print("resizing: ", str(filename))
+            try:
+                image_path = os.path.join(directory, filename)
+                image = cv2.imread(image_path)
+                resized_image = cv2.resize(image, (new_width, new_height), interpolation=cv2.INTER_AREA)
+                cv2.imwrite(image_path, resized_image)
+            except Exception as e:
+                print("Error: ", str(filename))
+                print(str(e))
 
 # Usage: python script.py <directory> <new_width> <new_height>
 if __name__ == '__main__':
     directory = sys.argv[1]
     new_width = int(sys.argv[2])
     new_height = int(sys.argv[3])
-    new_size = (new_width, new_height)
-    resize_images(directory, new_size)
-
-# Usage: python script.py <directory> <new_width> <new_height>
-if __name__ == '__main__':
-    directory = sys.argv[1]
-    new_width = int(sys.argv[2])
-    new_height = int(sys.argv[3])
-    new_size = (new_width, new_height)
-    resize_images(directory, new_size)
+    resize_images(directory, new_width, new_height)
+    print("Done resizing all images in the specified directory")
