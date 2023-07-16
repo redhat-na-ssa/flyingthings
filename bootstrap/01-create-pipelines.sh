0b670a9f36990571969fd2af66b43fcb1292f890
Super Dave <davwhite@davwhite.com>
2023-07-15 23:19:49 -0400

    removed namespaces

--- bootstrap/01-create-pipelines.sh
+++ bootstrap/01-create-pipelines.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
 
-oc new-project flyingthings-standalone
+if [ $# -eq 0 ]; then
+    echo "Warning: No tablespace provided. Please provide a target TABLESPACE name."
+    exit 1
+fi
+
+# Assign the first argument to the TABLESPACE variable
+TABLESPACE=$1
+
+oc new-project $TABLESPACE
 
 oc apply -f ../pipelines/tasks
 oc apply -f ../pipelines/manifests
+
+
+# Exit the script gracefully
+exit 0
 
