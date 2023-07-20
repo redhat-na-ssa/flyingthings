057eac992c821c42a76b5d9636fe96bb5f7a29af
David White <davwhite@davwhite.com>
2023-07-20 01:45:51 -0400

    end2end

--- bootstrap/10-run-model-flyingthings-training.sh
+++ bootstrap/10-run-model-flyingthings-training.sh
@@ -28,7 +28,7 @@ tkn pipeline start training-x-pipeline \
   -p MINIO_SECRETKEY=minioadmin \
   -p MINIO_BUCKET=flyingthings \
   -p MODEL_NAME=model-flyingthings \
-  -p MINIO_CLIENT_URL=util02.davenet.local \
+  -p MINIO_CLIENT_URL=https://dl.min.io/client/mc/release/linux-amd64 \
   -p DEPLOY="Y" \
   --use-param-defaults --showlog
 
