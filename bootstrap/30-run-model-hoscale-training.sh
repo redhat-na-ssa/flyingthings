f918585d9c6f19034a88650ae28a1f64b6923fb9
David White <davwhite@davwhite.com>
2023-07-20 01:45:51 -0400

    end2end

--- bootstrap/30-run-model-hoscale-training.sh
+++ bootstrap/30-run-model-hoscale-training.sh
@@ -27,7 +27,7 @@ tkn pipeline start training-x-pipeline \
   -p MINIO_SECRETKEY=minioadmin \
   -p MINIO_BUCKET=hoscale \
   -p MODEL_NAME=model-hoscale \
-  -p MINIO_CLIENT_URL=util02.davenet.local \
+  -p MINIO_CLIENT_URL=https://dl.min.io/client/mc/release/linux-amd64 \
   -p DEPLOY="Y" \
   --use-param-defaults --showlog
 
--- source/minio-client/02-create-bucket.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-MINCFG=miniocfg
-./mc --config-dir ${MINCFG} mb myminio/$MINIO_BUCKET --insecure
-./mc --config-dir ${MINCFG} version enable myminio/$MINIO_BUCKET --insecure
