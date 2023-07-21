886d238a3dca9592fc17a908b692be82fa1e040e
Super Dave <davwhite@davwhite.com>
2023-07-21 16:36:39 -0400

    get dataset

--- source/training/get-dataset.sh
+++ source/training/get-dataset.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -v
+#!/bin/bash
 cd $SIMPLEVIS_DATA/workspace
 curl $MINIO_CLIENT_URL/mc -o mc
 chmod +x mc
@@ -9,3 +9,16 @@ mkdir -p $SIMPLEVIS_DATA/workspace/datasets
 cd $SIMPLEVIS_DATA/workspace/datasets
 unzip $SIMPLEVIS_DATA/workspace/$DATASET_ZIP
 ls -l $SIMPLEVIS_DATA/workspace/datasets
+
+# Check if the file exists in the bucket
+if mc ls myminio/$MINIO_BUCKET/training-run.txt &> /dev/null; then
+    echo "File exists. Downloading..."
+    # Download the file
+    myminio/$MINIO_BUCKET/training-run.txt $SIMPLEVIS_DATA/workspace/
+else
+    echo "File does not exist. Creating..."
+    # Create the file in the bucket
+    echo "0" > training-run.txt
+    mc cp training-run.txt myminio/$MINIO_BUCKET/training-run.txt
+fi
+
