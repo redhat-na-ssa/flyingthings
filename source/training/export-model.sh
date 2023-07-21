f97d4b1e861e3d11bfbf5eea74a1780b6bfb49c9
David White <davwhite@davwhite.com>
2023-07-21 02:59:27 -0400

    torchscript export

--- source/training/export-model.sh
+++ source/training/export-model.sh
@@ -1,16 +1,3 @@
 #!/bin/bash
 cd $SIMPLEVIS_DATA/workspace
-yolo export model=${SIMPLEVIS_DATA}/workspace/$BASE_MODEL format=torchscript
-
-# Set the filename with .pt extension
-file_name=$BASE_MODEL
-
-# Check if the file exists
-if [ -f "$file_name" ]; then
-    # Replace .pt with .torchscript in the filename
-    new_name="${file_name%.pt}.torchscript"
-    mv "$file_name" "$new_name"
-    echo "Renamed '$file_name' to '$new_name'"
-else
-    echo "File '$file_name' not found."
-fi
+yolo export model=${SIMPLEVIS_DATA}/workspace/runs/train/weights/best.pt format=torchscript
