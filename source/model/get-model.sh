33ac3e9d974e34f6791e80ef1b233061a46d8a3f
Super Dave <davwhite@davwhite.com>
2023-07-23 15:24:59 -0400

    model server pulls latest tagged model file

--- source/model/get-model.sh
+++ source/model/get-model.sh
@@ -3,7 +3,20 @@ cd /opt/app-root/src/simplevis-data
 curl https://dl.min.io/client/mc/release/linux-amd64/mc -o mc
 chmod +x mc
 ./mc --config-dir=/opt/app-root/src/simplevis-data/mconfig config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
-./mc --config-dir=/opt/app-root/src/simplevis-data/mconfig cp myminio/$MINIO_BUCKET/$WEIGHTS $WEIGHTS --insecure
+
+# Get all model files from the latest training run
+LATEST_MOD_FILES=$(./mc --config-dir miniocfg find myminio/$MINIO_BUCKET/models --tags "training-run=latest" --insecure)
+
+# Loop through the file list and check for the pytorch model file
+for file in $LATEST_MOD_FILES; do
+echo "$COMMAND $file"
+    if [[ "$file" == *.pt ]]; then
+        echo "Using pytorch model file: $file"
+        ./mc --config-dir=/opt/app-root/src/simplevis-data/mconfig cp $file $WEIGHTS --insecure
+    fi
+    
+done
+
 rm ./mc
 cd /opt/app-root/src
 /usr/local/bin/uvicorn main:app --host 0.0.0.0
