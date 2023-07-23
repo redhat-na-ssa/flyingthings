94f5c62432c83666f567d0676f5abc56792ae7b5
Super Dave <davwhite@davwhite.com>
2023-07-23 18:37:02 -0400

    model server pulls latest tagged model file

--- source/model/get-model.sh
+++ source/model/get-model.sh
@@ -6,19 +6,26 @@ chmod +x mc
 MCONFIG=/opt/app-root/src/simplevis-data/mconfig
 ./mc --config-dir=$MCONFIG config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
 
-# Get all model files from the latest training run
-LATEST_MOD_FILES=$(./mc --config-dir $MCONFIG find myminio/$MINIO_BUCKET/models --tags "training-run=latest" --insecure)
-echo "Latest model files: $LATEST_MOD_FILES"
+# If BASE_MODEL is pretrained, use the pretrained pytorch model file
+if [ "$BASE_MODEL" == "model_pretrained.pt" ]; then
+  echo "Using pretrained model..."
+  ./mc --config-dir=$MCONFIG cp  myminio/$MINIO_BUCKET/$model_pretrained $WEIGHTS --insecure
+else
+  echo "Using custom model..."
 
-# Loop through the file list and check for the pytorch model file
-for file in $LATEST_MOD_FILES; do
-    echo "$COMMAND $file"
-        if [[ "$file" == *.pt ]]; then
-            echo "Using pytorch model file: $file"
-            ./mc --config-dir=$MCONFIG cp $file $WEIGHTS --insecure
-        fi
-    
-done
+  # Get all model files from the latest training run
+  LATEST_MOD_FILES=$(./mc --config-dir $MCONFIG find myminio/$MINIO_BUCKET/models --tags "training-run=latest" --insecure)
+  echo "Latest model files: $LATEST_MOD_FILES"
+  
+  # Loop through the file list and check for the pytorch model file
+  for file in $LATEST_MOD_FILES; do
+      echo "$COMMAND $file"
+          if [[ "$file" == *.pt ]]; then
+              echo "Using pytorch model file: $file"
+              ./mc --config-dir=$MCONFIG cp $file $WEIGHTS --insecure
+          fi
+  done
+fi
 
 rm ./mc
 cd /opt/app-root/src
