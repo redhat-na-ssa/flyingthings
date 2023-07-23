f6e971b5c2cf6a3cc583e3d98a9c2ed22b41dc14
Super Dave <davwhite@davwhite.com>
2023-07-23 15:54:27 -0400

    model server pulls latest tagged model file

--- source/model/get-model.sh
+++ source/model/get-model.sh
@@ -3,10 +3,11 @@ set -x
 cd /opt/app-root/src/simplevis-data
 curl https://dl.min.io/client/mc/release/linux-amd64/mc -o mc
 chmod +x mc
-./mc --config-dir=/opt/app-root/src/simplevis-data/mconfig config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
+MCONFIG=/opt/app-root/src/simplevis-data/mconfig
+./mc --config-dir=$MCONFIG config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
 
 # Get all model files from the latest training run
-LATEST_MOD_FILES=$(./mc --config-dir miniocfg find myminio/$MINIO_BUCKET/models --tags "training-run=latest" --insecure)
+LATEST_MOD_FILES=$(./mc --config-dir $MCONFIG find myminio/$MINIO_BUCKET/models --tags "training-run=latest" --insecure)
 echo "Latest model files: $LATEST_MOD_FILES"
 
 # Loop through the file list and check for the pytorch model file
@@ -14,7 +15,7 @@ for file in $LATEST_MOD_FILES; do
     echo "$COMMAND $file"
         if [[ "$file" == *.pt ]]; then
             echo "Using pytorch model file: $file"
-            ./mc --config-dir=/opt/app-root/src/simplevis-data/mconfig cp $file $WEIGHTS --insecure
+            ./mc --config-dir=$MCONFIG cp $file $WEIGHTS --insecure
         fi
     
 done
