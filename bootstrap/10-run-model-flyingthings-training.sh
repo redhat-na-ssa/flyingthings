ffce29db56bc3c346f8e0cf62329ced6fdcca97a
David White <davwhite@davwhite.com>
2023-07-23 13:09:07 -0400

    djw params

--- bootstrap/10-run-model-flyingthings-training.sh
+++ bootstrap/10-run-model-flyingthings-training.sh
@@ -17,7 +17,7 @@ tkn pipeline start training-x-pipeline \
   -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
   -p ocp-tablespace="$TABLESPACE" \
   -p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
-  -p git-revision=main \
+  -p git-revision=djw \
   -p GPU="N" \
   -p BATCH_SIZE="2" \
   -p NUM_EPOCHS="1" \
