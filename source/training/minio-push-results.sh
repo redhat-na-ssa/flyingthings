a1f7b8df8499e26091017f53c573b163c398d04b
Super Dave <davwhite@davwhite.com>
2023-07-23 15:00:12 -0400

    tag the previsous model and set new to latest

--- source/training/minio-push-results.sh
+++ source/training/minio-push-results.sh
@@ -9,39 +9,50 @@ ls -l $SIMPLEVIS_DATA
 echo "trainingrun: $trainingrun"
 ./mc --config-dir miniocfg config host add myminio $MINIO_ENDPOINT $MINIO_ACCESSKEY $MINIO_SECRETKEY --insecure
 
+PREVIOUS_RUN=0000
+CURRENT_RUN=0000
+
 # Get previous training run if it exists, otherwise set it to 0
-TRAINING_RUN_NUM=0
-TRAINING_RUN=$(./mc --config-dir miniocfg tag list --json myminio/$MINIO_BUCKET/model_custom.pt --insecure)
-training_run_value=$(echo "$TRAINING_RUN" | jq -r '.tagset."training-run"')
-echo "Previous training-run: $training_run_value"
+# First, list all objects with the tag "training-run=latest"
+LATEST_MOD_FILES=$(./mc --config-dir miniocfg find myminio/$MINIO_BUCKET --tags "training-run=latest" --insecure)
 
-# Store previous training run in a variable and tag previous objects
-PRIOR_TRAINING_RUN=$training_run_value
-./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/model_custom.pt "training-run=$PRIOR_TRAINING_RUN" --insecure
-./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/model_custom.torchscript "training-run=$PRIOR_TRAINING_RUN" --insecure
-./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/classes.txt "training-run=$PRIOR_TRAINING_RUN" --insecure
+# Check if any objects are returned
+if [ -n "$LATEST_MOD_FILES" ]; then
+    first_file="${LATEST_MOD_FILES[0]}"
 
-# Check if the value is null and set TRAINING_RUN_NUM accordingly
-if [ "$training_run_value" = "null" ]; then
-    TRAINING_RUN_NUM=0
+    # Get the file extension using parameter expansion
+    # This will extract everything after the last dot (.) in the filename
+    file_extension="${first_file##*_}"
+    run_number="${file_extension%.*}"
+
+    echo "First file: $first_file"
+    echo "File extension: $file_extension"
+    echo "Run number: $run_number"
+    PREVIOUS_RUN=$run_number
+    RUN_VALUE=$((run_number + 1))
+    CURRENT_RUN=$(printf "%04d" "$RUN_VALUE")
+
+    # Tag the previous run files with the previous run number
+    for file in $LATEST_MOD_FILES; do
+    echo "$COMMAND $file"
+        ./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/$file "training-run=$PREVIOUS_RUN" --insecure
+    done
 else
-    # If the value is not null, interpret it as a number and increment by 1
-    TRAINING_RUN_NUM=$((training_run_value + 1))
+  echo "No files found."
 fi
-#padded_number=$(printf "%04d" "$TRAINING_RUN_NUM")
-PADDED_NUMBER=$(printf "%04d" "$TRAINING_RUN_NUM")
-echo "This training run number: $PADDED_NUMBER"  
+echo "Current run: $CURRENT_RUN"
 
+# Push the results to minio
+# Push the training results to a training run folder
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/training-results.tgz myminio/$MINIO_BUCKET/training-run-$CURRENT_RUN/training-results.tgz --insecure
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/$MINIO_BUCKET/training-run-$CURRENT_RUN/$WEIGHTS --insecure
 
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/training-results.tgz myminio/$MINIO_BUCKET/training-run-$PADDED_NUMBER/training-results.tgz --insecure
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/$MINIO_BUCKET/training-run-$PADDED_NUMBER/$WEIGHTS --insecure
-# TODO: Add a check to see if the training run was successful
+# Push the latest model files to the root of the bucket
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/$MINIO_BUCKET/model_custom_$CURRENT_RUN.pt --insecure
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.torchscript myminio/$MINIO_BUCKET/model_custom_$CURRENT_RUN.torchscript --insecure
+./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/datasets/classes.txt myminio/$MINIO_BUCKET/classes_$CURRENT_RUN.txt --insecure
 
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.pt myminio/$MINIO_BUCKET/model_custom_$PADDED_NUMBER.pt --insecure
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/runs/train/weights/best.torchscript myminio/$MINIO_BUCKET/model_custom_$PADDED_NUMBER.torchscript --insecure
-./mc --config-dir miniocfg cp $SIMPLEVIS_DATA/workspace/datasets/classes.txt myminio/$MINIO_BUCKET/classes_$PADDED_NUMBER.txt --insecure
-
-# Set the training run for all objects
-./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/model_custom_$PADDED_NUMBER.pt "training-run=latest" --insecure
-./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/model_custom_$PADDED_NUMBER.torchscript "training-run=latest" --insecure
-./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/classes_$PADDED_NUMBER.txt "training-run=latest" --insecure
+# Set the training run tag to latest
+./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/model_custom_$CURRENT_RUN.pt "training-run=latest" --insecure
+./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/model_custom_$CURRENT_RUN.torchscript "training-run=latest" --insecure
+./mc --config-dir miniocfg tag set myminio/$MINIO_BUCKET/classes_$CURRENT_RUN.txt "training-run=latest" --insecure
