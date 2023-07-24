f9ae124ad6d7cbdfe5a0215657fcf17cece1a409
David White <davwhite@davwhite.com>
2023-07-24 17:46:14 -0400

    flag for deploying labelstudio

--- bootstrap/02-build-images-tkn.sh
+++ bootstrap/02-build-images-tkn.sh
@@ -13,10 +13,7 @@ echo "Deploying project to $TABLESPACE"
 tkn pipeline start flyingthings-images-pipeline \
   -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
   -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
-<<<<<<< HEAD
-=======
   -p git-url="https://github.com/redhat-na-ssa/flyingthings.git" \
->>>>>>> djw
   -p git-revision="main" \
   -p YOLO_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
   -p UBI_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
