4350411686082b0d0d8387d419974eacfa4f0a3d
David White <davwhite@davwhite.com>
2023-07-29 10:08:09 -0400

    model export

--- bootstrap/02-build-images-tkn.sh
+++ bootstrap/02-build-images-tkn.sh
@@ -20,6 +20,7 @@ tkn pipeline start flyingthings-images-pipeline \
   -p MINIMAL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
   -p CUSTOM_NOTEBOOK_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo-notebook:latest" \
   -p MODEL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/custom-model:latest" \
+  -p BASE_MODEL="yolov5su.pt" \
   -p MINIMAL_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
   -p MODEL_BUILD_ARGS="--build-arg WEIGHTS=flyingthings.pt --build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
   -p CUSTOM_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
--- bootstrap/10-run-model-flyingthings-training.sh
+++ bootstrap/10-run-model-flyingthings-training.sh
@@ -18,9 +18,10 @@ tkn pipeline start training-x-pipeline \
   -p ocp-tablespace="$TABLESPACE" \
   -p git-url=https://github.com/redhat-na-ssa/flyingthings.git \
   -p git-revision=main \
-  -p GPU="N" \
-  -p BATCH_SIZE="2" \
-  -p NUM_EPOCHS="1" \
+  -p GPU="Y" \
+  -p BASE_MODEL="yolov5su.pt" \
+  -p BATCH_SIZE="-1" \
+  -p NUM_EPOCHS="100" \
   -p IMG_RESIZE="Y" \
   -p MAX_WIDTH="200" \
   -p WEIGHTS=flyingthings.pt \
