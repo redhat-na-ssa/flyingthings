b2103459cd12fc857c27cbb1893d0693b7610180
David White <davwhite@davwhite.com>
2023-07-29 21:55:13 -0400

    replace yolov8 with yolov5

--- bootstrap/02-build-images-tkn.sh
+++ bootstrap/02-build-images-tkn.sh
@@ -9,25 +9,27 @@ fi
 TABLESPACE=$1
 
 # Execute your code here using the TABLESPACE variable
+# Hosted Copy  -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64" \
+# Local Copy  -p MINIO_CLIENT_URL="htt://util02.davenet.local" \
 echo "Deploying project to $TABLESPACE"
 tkn pipeline start flyingthings-images-pipeline \
   -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
   -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
   -p git-url="https://github.com/redhat-na-ssa/flyingthings.git" \
-  -p git-revision="main" \
+  -p git-revision="djw" \
   -p YOLO_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
   -p UBI_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
   -p MINIMAL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
   -p CUSTOM_NOTEBOOK_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo-notebook:latest" \
   -p MODEL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/custom-model:latest" \
-  -p BASE_MODEL="yolov5su.pt" \
+  -p BASE_MODEL="yolov5s.pt" \
   -p MINIMAL_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
   -p MODEL_BUILD_ARGS="--build-arg WEIGHTS=flyingthings.pt --build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
   -p CUSTOM_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
   -p MINIO_BUCKET="flyingthings" \
   -p MINIO_ACCESSKEY="minioadmin" \
   -p MINIO_SECRETKEY="minioadmin" \
-  -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64" \
+  -p MINIO_CLIENT_URL="htt://util02.davenet.local" \
   -p ocp-tablespace="$TABLESPACE" \
   -p DEPLOY_LABELSTUDIO="N" \
   --use-param-defaults --showlog
--- source/training/00-get-yolo-model.sh
+++ source/training/00-get-yolo-model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-yolo predict model=$BASE_MODEL
+curl -o $BASE_MODEL https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5s.pt
