66c1d99cf84ddff585cbef693594dc196da67278
David White <davwhite@davwhite.com>
2023-07-29 23:22:22 -0400

    replace yolov8 with yolov5

--- source/training/00-get-yolo-model.sh
+++ source/training/00-get-yolo-model.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -o $BASE_MODEL https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5s.pt
+wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5s.pt\
