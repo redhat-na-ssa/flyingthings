4263fa6d6cebd533b2d5fe74d65bf386c43db8ee
David White <davwhite@davwhite.com>
2023-08-01 22:32:52 -0400

    training with v5

--- source/training/03-run-training.sh
+++ source/training/03-run-training.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-git config --global -add safe.directory /usr/local/lib/python3.9/site-packages/yolov5
+git config --global --add safe.directory /usr/local/lib/python3.9/site-packages/yolov5
 cd $SIMPLEVIS_DATA/workspace
 # yolo train model=${SIMPLEVIS_DATA}/workspace/$BASE_MODEL batch=$BATCH_SIZE epochs=$NUM_EPOCHS data=classes.yaml project=${SIMPLEVIS_DATA}/workspace/runs exist_ok=True
 python3 /usr/local/lib/python3.9/site-packages/yolov5/train.py --img 640 --epochs 3 --data classes.yaml --weights ${SIMPLEVIS_DATA}/workspace/$BASE_MODEL
