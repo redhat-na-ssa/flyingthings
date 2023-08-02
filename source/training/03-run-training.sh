1fdaeef1505281b95e730ee5e9fa392b9c23008d
David White <davwhite@davwhite.com>
2023-08-01 23:52:10 -0400

    training with v5

--- source/training/03-run-training.sh
+++ source/training/03-run-training.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -x
 cd $SIMPLEVIS_DATA/workspace
-cp -R datasets/training /usr/local/lib/python3.9/site-packages/yolov5/training
+cp -R datasets/training/* /usr/local/lib/python3.9/site-packages/yolov5/training
 ls -l /usr/local/lib/python3.9/site-packages/yolov5
 ls -l /usr/local/lib/python3.9/site-packages/yolov5/training
 # yolo train model=${SIMPLEVIS_DATA}/workspace/$BASE_MODEL batch=$BATCH_SIZE epochs=$NUM_EPOCHS data=classes.yaml project=${SIMPLEVIS_DATA}/workspace/runs exist_ok=True
