d70f190a889d3055a81136b30dbb28f7be5d6a53
David White <davwhite@davwhite.com>
2023-08-02 00:10:03 -0400

    training with v5

--- source/training/export-model.sh
+++ source/training/export-model.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd $SIMPLEVIS_DATA/workspace
-yolo export model=${SIMPLEVIS_DATA}/workspace/runs/train/weights/best.pt format=onnx
+# yolo export model=${SIMPLEVIS_DATA}/workspace/runs/train/weights/best.pt format=onnx
+python /usr/local/lib/python3.9/site-packages/yolov5/export.py --weights ${SIMPLEVIS_DATA}/workspace/runs/train/weights/best.pt --include onnx
