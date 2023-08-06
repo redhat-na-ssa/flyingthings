5ce6612d7bbe6319bd6cb854b576eaad28bc237a
David White <davwhite@davwhite.com>
2023-08-06 08:52:09 -0400

    label studio import

--- source/labelstudio/entrypoint.sh
+++ source/labelstudio/entrypoint.sh
@@ -5,7 +5,7 @@ cd /opt/app-root/src/.local
 # Download the label studio export from google drive
 mkdir -p /opt/app-root/src/.local/share
 cd /opt/app-root/src/.local/share
-fileid="1gFT2sRdipkWOpxVnnzsWNcT3USB3LMNe"
+fileid="16qq4YFPmU2rQ7ZPRfqL0oBmOCz_Dnbp7"
 filename="label-studio.tar.gz"
 curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}" > /dev/null
 curl -Lb ./cookie "https://drive.google.com/uc?export=download&confirm=`awk '/download/ {print $NF}' ./cookie`&id=${fileid}" -o ${filename}
