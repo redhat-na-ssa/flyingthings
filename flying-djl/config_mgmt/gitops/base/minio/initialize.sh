37bf8e725f339130a7e0d6bf9e070b4d3d515990
jbride <jbride2001@yahoo.com>
2023-08-16 09:16:33 -0600

    djl: one more update to initialize.sh

--- flying-djl/config_mgmt/gitops/base/minio/initialize.sh
+++ flying-djl/config_mgmt/gitops/base/minio/initialize.sh
@@ -10,7 +10,7 @@ mc --config-dir /tmp event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:
 mc --config-dir /tmp event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".zip"
 mc --config-dir /tmp event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".pt"
 
-mc --config-dir tmp mb rht/flyingthings-correctivecandidates
-mc --config-dir tmp policy set public rht/flyingthings-correctivecandidates
+mc --config-dir /tmp mb rht/models-correctivecandidates
+mc --config-dir /tmp policy set public rht/models-correctivecandidates
 
 mc --config-dir /tmp admin service restart rht
