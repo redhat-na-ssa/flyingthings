5d5be0d81e9b34d8fb9d6956e6dfb96d0f0c0c68
jbride <jbride2001@yahoo.com>
2023-08-16 01:08:23 -0600

    djl:  fixed minio openshift deployment issue

--- flying-djl/config_mgmt/gitops/base/minio/initialize.sh
+++ flying-djl/config_mgmt/gitops/base/minio/initialize.sh
@@ -1,15 +1,16 @@
-sleep 10
+sleep 15 
 
-mc --config-dir /data alias set rht http://minio:9000/ $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
-mc --config-dir /data mb rht/flyingthings-models
-mc --config-dir /data policy set public rht/flyingthings-models
-mc --config-dir /data admin user add rht/ bucketwriter minio123
 
-mc --config-dir /data event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".txt"
-mc --config-dir /data event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".zip"
-mc --config-dir /data event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".pt"
+mc --config-dir /tmp alias set rht http://localhost:9000/ $MINIO_ROOT_USER $MINIO_ROOT_PASSWORD
+mc --config-dir /tmp mb rht/flyingthings-models
+mc --config-dir /tmp policy set public rht/flyingthings-models
+mc --config-dir /tmp admin user add rht/ bucketwriter minio123
 
-mc mb rht/flyingthings-correctivecandidates
-mc policy set public rht/flyingthings-correctivecandidates
+mc --config-dir /tmp event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".txt"
+mc --config-dir /tmp event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".zip"
+mc --config-dir /tmp event add  rht/flyingthings-models arn:minio:sqs::MLNOTIFY:mqtt --event "put,delete" --suffix ".pt"
 
-mc --config-dir /data admin service restart rht
+mc --config-dir tmp mb rht/flyingthings-correctivecandidates
+mc --config-dir tmp policy set public rht/flyingthings-correctivecandidates
+
+mc --config-dir /tmp admin service restart rht
