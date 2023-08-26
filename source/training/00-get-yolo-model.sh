5a3ac135e5a3f7565a0354802fea8d884d40651b
null <null>
2023-08-26 12:09:02 +0000

    pretrained bucket for yolo

--- source/training/00-get-yolo-model.sh
+++ source/training/00-get-yolo-model.sh
@@ -3,4 +3,4 @@
 wget https://github.com/ultralytics/yolov5/releases/download/v7.0/yolov5s.pt
 
 MINCFG=miniocfg
-./mc --config-dir ${MINCFG} cp myminio/$MINIO_BUCKET/model_pretrained_classes.yaml coco128.yaml  --insecure
+./mc --config-dir ${MINCFG} cp myminio/$PRETRAINED_BUCKET/model_pretrained_classes.yaml coco128.yaml  --insecure
