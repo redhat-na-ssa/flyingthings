d83d53c38363d86b3a3815388e1cf7b14fb6e865
Cory Latschkowski <koree@redhat.com>
2023-09-12 09:18:11 -0500

    update: scripts

--- bootstrap/02-start-pipelines.sh
+++ bootstrap/02-start-pipelines.sh
@@ -1,42 +1,51 @@
 #!/bin/bash
 
-if [ $# -eq 0 ]; then
-    echo Warning: No project provided. Please provide a target project.
-    exit 1
-fi
+select_namespace(){
+  if [ $# -eq 0 ]; then
+    NAMESPACE=${1:-modemo}
+    echo "NOTICE: No namespace / project name provided"
+  fi
 
-# Assign the first argument to the TABLESPACE variable
-TABLESPACE=$1
+  echo "NAMESPACE: ${NAMESPACE}"
+  oc project "${NAMESPACE}" &>/dev/null || oc new-project "${NAMESPACE}"
+}
 
-oc project $1
+start_pipelines(){
+  
+  select_namespace
 
-# Execute your code here using the TABLESPACE variable
-# Hosted Copy  -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64" \
-# Local Copy  -p MINIO_CLIENT_URL="http://util02.davenet.local" \
-# -p git-url="https://github.com/redhat-na-ssa/flyingthings.git" \
-echo "Deploying project to $TABLESPACE"
-tkn pipeline start flyingthings-images-pipeline \
-  -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
-  -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
-  -p git-url="https://github.com/davwhite/flyingthings.git" \
-  -p git-revision="main" \
-  -p YOLO_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
-  -p UBI_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
-  -p MINIMAL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
-  -p CUSTOM_NOTEBOOK_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo-notebook:latest" \
-  -p MODEL_IMAGE="image-registry.openshift-image-registry.svc:5000/$TABLESPACE/custom-model:latest" \
-  -p BASE_MODEL="yolov5s.pt" \
-  -p MINIMAL_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/base-ubi9:latest" \
-  -p MODEL_BUILD_ARGS="--build-arg WEIGHTS=flyingthings.pt --build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/yolo:latest" \
-  -p CUSTOM_BUILD_ARGS="--build-arg BASE_IMAGE=image-registry.openshift-image-registry.svc:5000/$TABLESPACE/minimal-notebook:latest" \
-  -p MINIO_BUCKET="flyingthings" \
-  -p PRETRAINED_BUCKET="yolo" \
-  -p MINIO_ACCESSKEY="minioadmin" \
-  -p MINIO_SECRETKEY="minioadmin" \
-  -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64" \
-  -p ocp-tablespace="$TABLESPACE" \
-  -p DEPLOY_LABELSTUDIO="Y" \
-  --use-param-defaults --showlog
+  # Hosted -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64"
+  # Local  -p MINIO_CLIENT_URL="http://util02.davenet.local"
 
-# Exit the script gracefully
-exit 0
+  PIPELINE_NAME=flyingthings-images-pipeline
+  IMAGE_REGISTRY=image-registry.openshift-image-registry.svc:5000
+
+  echo "Starting pipeline: ${PIPELINE_NAME}"
+
+  tkn pipeline start "${PIPELINE_NAME}" \
+    -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
+    -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
+    -p git-url="https://github.com/redhat-na-ssa/flyingthings.git" \
+    -p git-revision="main" \
+    -p YOLO_IMAGE="${IMAGE_REGISTRY}/${NAMESPACE}/yolo:latest" \
+    -p UBI_IMAGE="${IMAGE_REGISTRY}/${NAMESPACE}/base-ubi9:latest" \
+    -p MINIMAL_IMAGE="${IMAGE_REGISTRY}/${NAMESPACE}/minimal-notebook:latest" \
+    -p CUSTOM_NOTEBOOK_IMAGE="${IMAGE_REGISTRY}/${NAMESPACE}/yolo-notebook:latest" \
+    -p MODEL_IMAGE="${IMAGE_REGISTRY}/${NAMESPACE}/custom-model:latest" \
+    -p BASE_MODEL="yolov5s.pt" \
+    -p MINIMAL_BUILD_ARGS="--build-arg BASE_IMAGE=${IMAGE_REGISTRY}/${NAMESPACE}/base-ubi9:latest" \
+    -p MODEL_BUILD_ARGS="--build-arg WEIGHTS=flyingthings.pt --build-arg BASE_IMAGE=${IMAGE_REGISTRY}/${NAMESPACE}/yolo:latest" \
+    -p CUSTOM_BUILD_ARGS="--build-arg BASE_IMAGE=${IMAGE_REGISTRY}/${NAMESPACE}/minimal-notebook:latest" \
+    -p MINIO_BUCKET="flyingthings" \
+    -p PRETRAINED_BUCKET="yolo" \
+    -p MINIO_ACCESSKEY="minioadmin" \
+    -p MINIO_SECRETKEY="minioadmin" \
+    -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64" \
+    -p ocp-tablespace="${NAMESPACE}" \
+    -p DEPLOY_LABELSTUDIO="Y" \
+    --use-param-defaults --showlog
+
+  # Exit the script gracefully
+}
+
+start_pipelines
