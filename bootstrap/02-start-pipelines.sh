cc2df8ff00255f011df07c319f618bb5eb7637c0
Cory Latschkowski <koree@redhat.com>
2023-09-12 10:09:53 -0500

    update: ocp-tablespace to NAMESPACE

--- bootstrap/02-start-pipelines.sh
+++ bootstrap/02-start-pipelines.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
 
-select_namespace(){
-  if [ $# -eq 0 ]; then
-    NAMESPACE=${1:-modemo}
-    echo "NOTICE: No namespace / project name provided"
-  fi
-
+check_namespace(){
+  NAMESPACE=$(oc project -q)
   echo "NAMESPACE: ${NAMESPACE}"
-  oc project "${NAMESPACE}" &>/dev/null || oc new-project "${NAMESPACE}"
+  echo ""
+  echo "NOTICE: Verify you are working in the correct namespace"
+  echo "Use CTRL + C to cancel"
+  sleep 8
 }
 
-start_pipelines(){
-  
-  select_namespace
 
-  # Hosted -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64"
-  # Local  -p MINIO_CLIENT_URL="http://util02.davenet.local"
-
-  PIPELINE_NAME=flyingthings-images-pipeline
-  IMAGE_REGISTRY=image-registry.openshift-image-registry.svc:5000
-
-  echo "Starting pipeline: ${PIPELINE_NAME}"
+debug_pipeline(){
 
   tkn pipeline start "${PIPELINE_NAME}" \
     -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
@@ -41,11 +31,46 @@ start_pipelines(){
     -p MINIO_ACCESSKEY="minioadmin" \
     -p MINIO_SECRETKEY="minioadmin" \
     -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64" \
-    -p ocp-tablespace="${NAMESPACE}" \
+    -p NAMESPACE="${NAMESPACE}" \
     -p DEPLOY_LABELSTUDIO="Y" \
     --use-param-defaults --showlog
+}
 
-  # Exit the script gracefully
+start_pipelines(){
+  
+  check_namespace
+
+  # Hosted -p MINIO_CLIENT_URL="https://dl.min.io/client/mc/release/linux-amd64"
+  # Local  -p MINIO_CLIENT_URL="http://util02.davenet.local"
+
+  PIPELINE_NAME=flyingthings-images-pipeline
+  IMAGE_REGISTRY=image-registry.openshift-image-registry.svc:5000
+  GIT_URL=https://github.com/redhat-na-ssa/flyingthings.git
+  GIT_REVISION=main
+
+  # check for pipeline in current namespace
+  if oc get pipeline "${PIPELINE_NAME}" -o name; then
+    echo "PIPELINE: ${PIPELINE_NAME} exists"
+  else
+    echo "PIPELINE: ${PIPELINE_NAME} missing"
+    exit 0
+  fi
+
+  echo "Starting pipeline: ${PIPELINE_NAME}"
+
+  # kludge
+  [ "${PWD##*/}" != "bootstrap" ] && pushd bootstrap
+
+  # debug_pipeline; exit 0
+
+  tkn pipeline start "${PIPELINE_NAME}" \
+    -p git-url="${GIT_URL}" \
+    -p git-revision="${GIT_REVISION}" \
+    -p NAMESPACE="${NAMESPACE}" \
+    -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
+    -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
+    --use-param-defaults --showlog
+
 }
 
 start_pipelines
