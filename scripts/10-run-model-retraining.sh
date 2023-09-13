db43815921697a3d4c55b84299205dba3e961dec
Cory Latschkowski <koree@redhat.com>
2023-09-13 11:34:04 -0500

    update: sourcecode to source

--- scripts/10-run-model-retraining.sh
+++ scripts/10-run-model-retraining.sh
@@ -37,7 +37,7 @@ start_pipelines(){
 
   check_pipeline model-retraining
   tkn pipeline start "${PIPELINE_NAME}" \
-    -w name=sourcecode,volumeClaimTemplateFile=code-pvc.yaml \
+    -w name=source,volumeClaimTemplateFile=code-pvc.yaml \
     -w name=shared-workspace,volumeClaimTemplateFile=work-pvc.yaml \
     -p GIT_URL="${GIT_URL}" \
     -p GIT_REVISION="${GIT_REVISION}" \
