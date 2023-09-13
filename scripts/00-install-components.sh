841586671a13eca45a5b6ad2fd9d88231f3a46bd
Cory Latschkowski <koree@redhat.com>
2023-09-12 22:10:44 -0500

    update: scripts

--- scripts/00-install-components.sh
+++ scripts/00-install-components.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
 
+select_namespace(){
+  if [ $# -eq 0 ]; then
+    NAMESPACE=${1:-modemo}
+    echo "NOTICE: No namespace / project name provided"
+  fi
+
+  echo "NAMESPACE: ${NAMESPACE}"
+  oc project "${NAMESPACE}" &>/dev/null || oc new-project "${NAMESPACE}"
+}
+
 setup_minio(){
+  
+  select_namespace
   oc apply -k components/demo/minio
 }
 
