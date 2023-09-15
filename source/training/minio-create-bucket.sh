9f3474ac40aa921a35826bf7443642c3422f0e31
Cory Latschkowski <koree@redhat.com>
2023-09-15 07:34:32 -0500

    update: only create one bucket

--- source/training/minio-create-bucket.sh
+++ source/training/minio-create-bucket.sh
@@ -4,5 +4,5 @@ MINCFG=miniocfg
 ./mc --config-dir ${MINCFG} version enable myminio/$MINIO_BUCKET --insecure
 
 # Create a bucket for the pre-trained model
-./mc --config-dir ${MINCFG} mb myminio/yolo --insecure
-./mc --config-dir ${MINCFG} version enable myminio/yolo --insecure
+# ./mc --config-dir ${MINCFG} mb myminio/yolo --insecure
+# ./mc --config-dir ${MINCFG} version enable myminio/yolo --insecure
