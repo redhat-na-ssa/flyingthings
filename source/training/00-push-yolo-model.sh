25b764111e2c4a68e62c62cafcb1383e9bb62237
Cory Latschkowski <koree@redhat.com>
2023-09-14 21:18:53 -0500

    delete: extra BUCKET params

--- source/training/00-push-yolo-model.sh
+++ source/training/00-push-yolo-model.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 MINCFG=miniocfg
-./mc --config-dir ${MINCFG} cp $BASE_MODEL myminio/$PRETRAINED_BUCKET/model_pretrained.pt --insecure
-./mc --config-dir ${MINCFG} cp coco128.yaml myminio/$PRETRAINED_BUCKET/model_pretrained_classes.yaml --insecure
+./mc --config-dir ${MINCFG} cp $BASE_MODEL myminio/$MINIO_BUCKET/pretrained/model_pretrained.pt --insecure
+./mc --config-dir ${MINCFG} cp coco128.yaml myminio/$MINIO_BUCKET/pretrained/model_pretrained_classes.yaml --insecure
