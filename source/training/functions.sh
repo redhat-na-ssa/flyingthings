65fa1129db9e4a7867533af714f8bf9a83809246
Cory Latschkowski <koree@redhat.com>
2023-09-15 21:37:29 -0500

    fix stuff

--- source/training/functions.sh
+++ source/training/functions.sh
@@ -46,8 +46,8 @@ minio_tag(){
 }
 
 minio_copy_artifacts(){
-  minio_copy "${SOURCE_DIR}/artifacts/flyingthings-yolo.zip" "${MINIO_REMOTE}/${MINIO_BUCKET}"
-  minio_copy "${SOURCE_DIR}/artifacts/coco128.yaml" "${MINIO_REMOTE}/${MINIO_BUCKET}/pretrained/model_pretrained_classes.yaml"
+  minio_copy "artifacts/flyingthings-yolo.zip" "${MINIO_REMOTE}/${MINIO_BUCKET}"
+  minio_copy "artifacts/coco128.yaml" "${MINIO_REMOTE}/${MINIO_BUCKET}/pretrained/model_pretrained_classes.yaml"
 }
 
 minio_push_results(){
