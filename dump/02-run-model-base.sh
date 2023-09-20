b97f2f05773d96ba5c9459f50febf70161ba0052
Cory Latschkowski <koree@redhat.com>
2023-09-20 07:24:56 -0500

    update: branch ref to develop

--- dump/02-run-model-base.sh
+++ dump/02-run-model-base.sh
@@ -25,7 +25,7 @@ start_pipelines(){
 
   IMAGE_REGISTRY=image-registry.openshift-image-registry.svc:5000
   GIT_URL=https://github.com/redhat-na-ssa/flyingthings.git
-  GIT_REVISION=merge-other-git
+  GIT_REVISION=develop
 
   # kludge
   [ "${PWD##*/}" != "scripts" ] && pushd scripts
