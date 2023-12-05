67a20dcc4e0e9fddc888b934a9477a9da1f4548f
Cory Latschkowski <koree@redhat.com>
2023-12-04 18:41:54 -0600

    update: scripts

--- components/label-studio/container/s2i/entrypoint.sh
+++ components/label-studio/container/s2i/entrypoint.sh
@@ -3,7 +3,11 @@ set -e
 
 kludge_to_newest(){
   label-studio version
+  
+  echo "kludging ahead to newest version..."
   pip install --no-cache -U label-studio -q
+  
+  label-studio version
 }
 
 kludge_to_newest
