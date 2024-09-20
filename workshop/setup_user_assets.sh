6db7b8daf14c9178067e7c1109696123c16cb160
David White <davwhite@davwhite.com>
2024-09-20 08:34:00 -0400

    share training imagestreams

--- workshop/setup_user_assets.sh
+++ workshop/setup_user_assets.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-# create assets in each user's project
+# Grant access to all the CV training image streams
 for i in $(seq -w 1 10); do
-  echo "Current user: user$i"
-  oc project user$i
-  workshop/00-user-components.sh
+  oc adm policy add-role-to-user view user$i -n ml-demo
+  oc adm policy add-role-to-user system:image-puller user$i -n ml-demo
 done
+
 
 for i in $(seq -w 1 10); do
   echo "Current user: user$i"
