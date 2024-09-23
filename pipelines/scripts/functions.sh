591478d23dcb50e776bbd17aaf8ac8b7f1f1c34f
David White <davwhite@davwhite.com>
2024-09-23 08:17:32 -0400

    user namespace

--- pipelines/scripts/functions.sh
+++ pipelines/scripts/functions.sh
@@ -224,7 +224,7 @@ check_base_image(){
 
   while [ -z "${READY}" ]
   do
-    oc get istag "${IS_TAG}" -o name 2>/dev/null && \
+    oc get istag "${IS_TAG}" -n "${MAIN_NAMESPACE}" -o name 2>/dev/null && \
     READY=true && continue
     sleep 10
   done
