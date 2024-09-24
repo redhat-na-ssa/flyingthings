74df83862bfebbb1a851ee4b9bbbf3192f6b11d7
David White <davwhite@davwhite.com>
2024-09-24 08:54:34 -0400

    individual user runs

--- pipelines/scripts/functions.sh
+++ pipelines/scripts/functions.sh
@@ -223,7 +223,7 @@ check_base_image(){
 
   while [ -z "${READY}" ]
   do
-    oc get istag "${IS_TAG}" -n ml-demo -o name 2>/dev/null && \
+    oc get istag "${IS_TAG}" -o name 2>/dev/null && \
     READY=true && continue
     sleep 10
   done
--- workshop/setup_user_assets.sh
+++ workshop/setup_user_assets.sh
@@ -2,12 +2,14 @@
 
 # Define the range of users and the GPU quota values
 START=1      # Starting user number (user01, user02, etc.)
-END=10       # Ending user number (user10, user11, etc.)
+END=2       # Ending user number (user10, user11, etc.)
 
 # Grant access to all the CV training image streams
 for i in $(seq -f "%02g" $START $END); do
   oc adm policy add-role-to-user view user$i -n ml-demo
   oc adm policy add-role-to-user system:image-puller user$i -n ml-demo
+  oc tag ml-demo/yolo-api:latest user$i/yolo-api:latest
+  oc tag ml-demo/yolo-api:latest user$i/model-yolo:latest
 done
 
 
@@ -15,4 +17,5 @@ for i in $(seq -f "%02g" $START $END); do
   echo "Current user: user$i"
   oc project user$i
   workshop/01-setup-pipelines.sh
+  oc apply -k gitops/02-workshop-user-components
 done
