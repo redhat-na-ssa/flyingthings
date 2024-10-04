e52df6b5c2285dfdd85bcb2eca4382a092c86b8c
Cory Latschkowski <koree@redhat.com>
2024-10-03 22:24:19 -0500

    update: job

--- gitops/00-cluster-config/adhoc-ops/00-job/job.sh
+++ gitops/00-cluster-config/adhoc-ops/00-job/job.sh
@@ -9,9 +9,15 @@ ocp_allow_router_on_control_plane(){
     --type=merge --patch '{"spec":{"nodePlacement":{"tolerations":[{"key":"node-role.kubernetes.io/master","operator":"Exists","effect":"NoSchedule"}]}}}'
 }
 
+ocp_allow_registry_on_control_plane(){
+  oc patch configs.imageregistry.operator.openshift.io/cluster \
+    --type=merge --patch '{"spec":{"tolerations":[{"key":"node-role.kubernetes.io/master","operator":"Exists","effect":"NoSchedule"}]}}'
+}
+
 ocp_aws_cluster || exit 0
 ocp_clean_install_pods
 ocp_scale_machineset 1
 ocp_control_nodes_not_schedulable
-ocp_set_scheduler_profile HighNodeUtilization
+# ocp_set_scheduler_profile HighNodeUtilization
 ocp_allow_router_on_control_plane
+ocp_allow_registry_on_control_plane
